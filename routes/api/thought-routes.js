4d78999927f721ce0a65b56372360a1ef7e28517
Owais Islam <owaisislam@gmail.com>
2020-12-02 17:32:35 -0600

    added user and thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,29 @@
 const router = require("express").Router();
+const {
+	getAllThoughts,
+	getThoughtById,
+	createThought,
+	updateThought,
+	deleteThought,
+	addReaction,
+	removeReaction,
+} = require("../../controllers/thought-controller");
+
+
+// /api/thoughts
+router.route("/").get(getAllThoughts);
+
+// /api/thoughts/<thoughtId>
+router.route("/:thoughtId").get(getThoughtById).put(updateThought);
+
+// /api/thoughts/<userId>
+router.route("/:userId").post(createThought);
+
+// /api/thoughts/<userId>/<thoughtId>
+router.route("/:userId/:thoughtId").post(addReaction).delete(deleteThought);
+
+// /api/thoughts/<userId>/<thoughtId>/<reactionId>
+router.route("/:userId/:thoughtId/:reactionId").delete(removeReaction);
+
 
 module.exports = router;
