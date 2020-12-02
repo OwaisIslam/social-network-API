ad247ad691045b7bc5eb076dbe9042a87ce574f0
Owais Islam <owaisislam@gmail.com>
2020-12-02 17:40:39 -0600

    added addReaction and removeReaction to thought controller

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -85,6 +85,32 @@ const thoughtController = {
 			})
 			.catch((err) => res.json(err));
 	},
+	// add a reaction
+	addReaction({ params, body }, res) {
+		Thought.findOneAndUpdate(
+			{ _id: params.thoughtId },
+			{ $push: { reactions: body } },
+			{ new: true, runValidators: true }
+		)
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No user found with this ID!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => res.json(err));
+	},
+	// remove a reaction
+	removeReaction({ params }, res) {
+		Thought.findOneAndUpdate(
+			{ _id: params.thoughtId },
+			{ $pull: { reactions: { reactionId: params.reactionId } } },
+			{ new: true }
+		)
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => res.json(err));
+	},
 };
 
 module.exports = thoughtController;
